37bafaa64ddac204447da39179ba7d9553043484
Atul R <atulanand94@gmail.com>
2019-06-06 18:40:52 +0200

    Fixes segmentation fault due to garbage collection of flex nodes

--- src/lib/core/FlexLayout/index.ts
+++ src/lib/core/FlexLayout/index.ts
@@ -5,11 +5,17 @@ import { NodeLayout } from "../../QtWidgets/QLayout";
 
 export class FlexLayout extends NodeLayout {
   native = new addon.FlexLayout();
+  protected flexNode?: FlexNode;
+  protected childFlexNodes = new Set<FlexNode>();
+
   addWidget(childWidget: NodeWidget, childFlexNode: FlexNode) {
     this.children.add(childWidget);
+    this.childFlexNodes.add(childFlexNode);
+
     this.native.addWidget(childWidget.native, childFlexNode.native);
   }
   setFlexNode(flexNode: FlexNode) {
+    this.flexNode = flexNode;
     this.native.setFlexNode(flexNode.native);
   }
 }
--- src/lib/core/YogaWidget/index.ts
+++ src/lib/core/YogaWidget/index.ts
@@ -3,7 +3,6 @@ import { FlexNode } from "../FlexLayout/FlexNode";
 
 export abstract class YogaWidget extends Component {
   getFlexNode(): FlexNode {
-    const nativeFlexNode = this.native.getFlexNode();
-    return new FlexNode(nativeFlexNode);
+    return new FlexNode(this.native.getFlexNode());
   }
 }
