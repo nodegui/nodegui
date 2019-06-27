e2a4046a348612f839478dd72890fdddb8612967
Atul R <atulanand94@gmail.com>
2019-06-27 19:21:09 +0200

    Added exports

---
+++ src/lib/index.ts
@@ -0,0 +1,18 @@
+export { QApplication } from "./QtGui/QApplication";
+export { QWidget } from "./QtGui/QWidget";
+// Abstract:
+export { NodeWidget } from "./QtGui/QWidget";
+export { NodeLayout } from "./QtWidgets/QLayout";
+// Widgets:
+export { QCheckBox } from "./QtWidgets/QCheckBox";
+export { QLabel } from "./QtWidgets/QLabel";
+export { QLineEdit } from "./QtWidgets/QLineEdit";
+export { QMainWindow } from "./QtWidgets/QMainWindow";
+export { QProgressBar } from "./QtWidgets/QProgressBar";
+export { QPushButton } from "./QtWidgets/QPushButton";
+export { QRadioButton } from "./QtWidgets/QRadioButton";
+// Layouts:
+export { QGridLayout } from "./QtWidgets/QGridLayout";
+export { FlexLayout } from "./core/FlexLayout";
+// Events : Maybe a separate module ?
+export { QKeyEvent } from "./QtGui/QEvent/QKeyEvent";
