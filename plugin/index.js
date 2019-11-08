ae81e2aae50bbf69863d144360436d35c17bda41
Atul R <atulanand94@gmail.com>
2019-11-08 20:39:08 +0100

    cleanup plugin.js

--- plugin/index.js
+++ plugin/index.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 
-const NODEGUI_ROOT = path.resolve(__dirname, "..");
-const CMAKE_HELPER_DIR = path.resolve(NODEGUI_ROOT, "config");
-const CMAKE_HELPER_FILE = path.resolve(CMAKE_HELPER_DIR, "plugin.cmake");
+const NODEGUI_ROOT = path.resolve(__dirname, '..');
+const CMAKE_HELPER_DIR = path.resolve(NODEGUI_ROOT, 'config');
+const CMAKE_HELPER_FILE = path.resolve(CMAKE_HELPER_DIR, 'plugin.cmake');
 
 module.exports = {
-  CMAKE_HELPER_FILE,
-  CMAKE_HELPER_DIR,
-  NODEGUI_ROOT
+    CMAKE_HELPER_FILE,
+    CMAKE_HELPER_DIR,
+    NODEGUI_ROOT,
 };
