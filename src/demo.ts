82d1a8b1fdc8bd0f9b48916f7d64f711a92190e0
Atul R <atulanand94@gmail.com>
2019-11-08 21:07:59 +0100

    Testing the flex layout a bit

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,33 @@
 import { QMainWindow } from './index';
+import { QWidget } from './lib/QtWidgets/QWidget';
+import { FlexLayout } from './lib/core/FlexLayout';
+import { QLabel } from './lib/QtWidgets/QLabel';
 
 const win = new QMainWindow();
+const view = new QWidget();
+view.setLayout(new FlexLayout());
+view.setStyleSheet(`
+    flex: 1;
+    width: '100%';
+    height: '100%';
+`);
+const hello = new QLabel();
+hello.setText('Hello');
+hello.setStyleSheet(`
+    border: 1px solid blue;
+    qproperty-alignment: AlignCenter;
+`);
+const world = new QLabel();
+world.setText('World');
+world.setStyleSheet(`
+    border: 1px solid blue;
+    qproperty-alignment: AlignCenter;
+`);
+if (view.layout) {
+    view.layout.addWidget(hello);
+    view.layout.addWidget(world);
+}
+win.setCentralWidget(view);
 win.show();
 
 (global as any).win = win; // To prevent win from being garbage collected.
