d4d3a15036c3a996d3da5df0b1949940ed607f2d
Atul R <atulanand94@gmail.com>
2019-11-18 00:42:12 +0100

    lint fix

--- src/cpp/include/nodegui/QtWidgets/QScrollArea/qscrollarea_wrap.h
+++ src/cpp/include/nodegui/QtWidgets/QScrollArea/qscrollarea_wrap.h
@@ -12,6 +12,7 @@ class QScrollAreaWrap : public Napi::ObjectWrap<QScrollAreaWrap> {
  private:
   QPointer<NScrollArea> instance;
   YGNodeRef scrollNode;
+
  public:
   static Napi::Object init(Napi::Env env, Napi::Object exports);
   QScrollAreaWrap(const Napi::CallbackInfo &info);
