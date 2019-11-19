dfdb1907c74541df039bc80a8a35d31d07b06da1
Atul R <atulanand94@gmail.com>
2019-11-19 01:31:22 +0100

    Adds size test for radiobutton and checkbox (#207)

--- src/lib/QtWidgets/__tests__/QRadioButton.test.ts
+++ src/lib/QtWidgets/__tests__/QRadioButton.test.ts
@@ -1,6 +1,7 @@
 import { QRadioButton } from '../QRadioButton';
 import { QIcon } from '../../QtGui/QIcon';
 import path from 'path';
+import { QSize } from '../../QtCore/QSize';
 
 describe('QRadioButton', () => {
     it('instantiate a button instance', () => {
@@ -18,5 +19,15 @@ describe('QRadioButton', () => {
         const icon = new QIcon(testImagePath);
         button.setIcon(icon);
         expect(QIcon.fromQVariant(button.property('icon')).cacheKey()).toEqual(icon.cacheKey());
+    });
+    it('setIconSize', () => {
+        const button = new QRadioButton();
+        const testImagePath = path.resolve(__dirname, 'assets', 'nodegui.png');
+        const icon = new QIcon(testImagePath);
+        button.setIcon(icon);
+        button.setIconSize(new QSize(111, 111));
+        const returnedSize = button.iconSize();
+        expect(returnedSize.width()).toBe(111);
+        expect(returnedSize.height()).toBe(111);
     });
 });
