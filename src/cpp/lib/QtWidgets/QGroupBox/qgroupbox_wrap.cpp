32a1b97012a526cf13e3ee59189e9ae8f59abea0
feng8848 <40539968+feng8848@users.noreply.github.com>
2019-12-08 01:29:17 +0800

    Add QGroupBox (#251)

---
+++ src/cpp/lib/QtWidgets/QGroupBox/qgroupbox_wrap.cpp
@@ -0,0 +1,129 @@
+#include "QtWidgets/QGroupBox/qgroupbox_wrap.h"
+
+#include <QWidget>
+
+#include "Extras/Utils/nutils.h"
+#include "QtWidgets/QWidget/qwidget_wrap.h"
+
+Napi::FunctionReference QGroupBoxWrap::constructor;
+
+Napi::Object QGroupBoxWrap::init(Napi::Env env, Napi::Object exports) {
+  Napi::HandleScope scope(env);
+  char CLASSNAME[] = "QGroupBox";
+  Napi::Function func = DefineClass(
+      env, CLASSNAME,
+      {InstanceMethod("alignment", &QGroupBoxWrap::alignment),
+       InstanceMethod("isCheckable", &QGroupBoxWrap::isCheckable),
+       InstanceMethod("isChecked", &QGroupBoxWrap::isChecked),
+       InstanceMethod("isFlat", &QGroupBoxWrap::isFlat),
+       InstanceMethod("setAlignment", &QGroupBoxWrap::setAlignment),
+       InstanceMethod("setCheckable", &QGroupBoxWrap::setCheckable),
+       InstanceMethod("setFlat", &QGroupBoxWrap::setFlat),
+       InstanceMethod("setTitle", &QGroupBoxWrap::setTitle),
+       InstanceMethod("title", &QGroupBoxWrap::title),
+       InstanceMethod("setChecked", &QGroupBoxWrap::setChecked),
+       QWIDGET_WRAPPED_METHODS_EXPORT_DEFINE(QGroupBoxWrap)});
+  constructor = Napi::Persistent(func);
+  exports.Set(CLASSNAME, func);
+  return exports;
+}
+
+NGroupBox* QGroupBoxWrap::getInternalInstance() { return this->instance; }
+
+QGroupBoxWrap::QGroupBoxWrap(const Napi::CallbackInfo& info)
+    : Napi::ObjectWrap<QGroupBoxWrap>(info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+  if (info.Length() == 1) {
+    Napi::Object parentObject = info[0].As<Napi::Object>();
+    QWidgetWrap* parentWidgetWrap =
+        Napi::ObjectWrap<QWidgetWrap>::Unwrap(parentObject);
+    this->instance = new NGroupBox(parentWidgetWrap->getInternalInstance());
+  } else if (info.Length() == 0) {
+    this->instance = new NGroupBox();
+  } else {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+  }
+  this->rawData = extrautils::configureQWidget(
+      this->getInternalInstance(), this->getInternalInstance()->getFlexNode(),
+      true);
+}
+
+QGroupBoxWrap::~QGroupBoxWrap() { extrautils::safeDelete(this->instance); }
+
+Napi::Value QGroupBoxWrap::alignment(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  int alignment = static_cast<int>(this->instance->alignment());
+  return Napi::Value::From(env, alignment);
+}
+
+Napi::Value QGroupBoxWrap::isCheckable(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool checkable = this->instance->isCheckable();
+  return Napi::Value::From(env, checkable);
+}
+
+Napi::Value QGroupBoxWrap::isChecked(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool checked = this->instance->isChecked();
+  return Napi::Value::From(env, checked);
+}
+
+Napi::Value QGroupBoxWrap::isFlat(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool flat = this->instance->isFlat();
+  return Napi::Value::From(env, flat);
+}
+
+Napi::Value QGroupBoxWrap::setAlignment(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env); 
+  int alignment = info[0].As<Napi::Number>().Int32Value();
+  this->instance->setAlignment(alignment);
+  return env.Null();
+}
+
+Napi::Value QGroupBoxWrap::setCheckable(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool checkable = info[0].As<Napi::Boolean>().Value();
+  this->instance->setCheckable(checkable);
+  return env.Null();
+}
+
+Napi::Value QGroupBoxWrap::setFlat(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool flat = info[0].As<Napi::Boolean>().Value();
+  this->instance->setFlat(flat);
+  return env.Null();
+}
+
+Napi::Value QGroupBoxWrap::setTitle(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  std::string text = info[0].As<Napi::String>().Utf8Value();    
+  this->instance->setTitle(text.c_str());
+  return env.Null();
+}
+
+Napi::Value QGroupBoxWrap::title(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  QString text = this->instance->title();
+  return Napi::String::New(env, text.toStdString().c_str());
+}
+
+Napi::Value QGroupBoxWrap::setChecked(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  bool checked = info[0].As<Napi::Boolean>().Value();
+  this->instance->setChecked(checked);
+  return env.Null();
+}
---
+++ src/lib/QtWidgets/QGroupBox.ts
@@ -0,0 +1,55 @@
+import addon from '../utils/addon';
+import { NodeWidget } from './QWidget';
+import { BaseWidgetEvents } from '../core/EventWidget';
+import { NativeElement } from '../core/Component';
+import { AlignmentFlag } from '../QtEnums/AlignmentFlag';
+
+export const QGroupBoxEvents = Object.freeze({
+    ...BaseWidgetEvents,
+    clicked: 'clicked',
+    toggled: 'toggled',
+});
+export class QGroupBox extends NodeWidget {
+    native: NativeElement;
+    constructor(parent?: NodeWidget) {
+        let native;
+        if (parent) {
+            native = new addon.QGroupBox(parent.native);
+        } else {
+            native = new addon.QGroupBox();
+        }
+        super(native);
+        this.native = native;
+        this.nodeParent = parent;
+    }
+    alignment(): AlignmentFlag {
+        return this.native.alignment();
+    }
+    isCheckable(): boolean {
+        return this.native.isCheckable();
+    }
+    isChecked(): boolean {
+        return this.native.isChecked();
+    }
+    isFlat(): boolean {
+        return this.native.isFlat();
+    }
+    setAlignment(alignment: number): void {
+        this.native.setAlignment(alignment);
+    }
+    setCheckable(checkable: boolean): void {
+        this.native.setCheckable(checkable);
+    }
+    setFlat(flat: boolean): void {
+        this.native.setFlat(flat);
+    }
+    setTitle(title: string): void {
+        this.native.setTitle(title);
+    }
+    title(): string {
+        return this.native.title();
+    }
+    setChecked(checked: boolean): void {
+        this.native.setChecked(checked);
+    }
+}
