a3aa6ea44a1cb51fb5fdf4b2769879fbf73689db
slidinghotdog <33790211+slidinghotdog@users.noreply.github.com>
2019-12-08 15:25:59 -0300

    add  addItems() and insertItems() to QComboBox (#253)

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,4 @@
-import { QMainWindow, QPushButton, QLabel, QWidget, FlexLayout } from './index';
+import { QMainWindow, QPushButton, QLabel, QWidget, FlexLayout, QComboBox, QComboBoxEvents } from './index';
 
 const win = new QMainWindow();
 const center = new QWidget();
@@ -9,9 +9,15 @@ button.setText('Hello');
 const hello = new QLabel();
 hello.setText('hello text');
 
+const combo = new QComboBox();
+combo.addItems(['text1', 'text2', 'text3'])
+combo.insertItems(1,['insert1', 'insert2', 'insert3'])
+combo.addEventListener(QComboBoxEvents.currentTextChanged, (text)=>hello.setText(text))
+
 center.setLayout(new FlexLayout());
 center.layout?.addWidget(button);
 center.layout?.addWidget(hello);
+center.layout?.addWidget(combo);
 
 win.setCentralWidget(center);
 win.show();
