eb1a302ec46af9c7dec9dcaab574e5d46392edf0
Atul R <atulanand94@gmail.com>
2020-01-09 21:38:15 +0530

    Adds QGroupBox

--- src/lib/QtWidgets/QGroupBox.ts
+++ src/lib/QtWidgets/QGroupBox.ts
@@ -14,10 +14,29 @@ A `QGroupBox` widget provides a group box frame with a title.
 ### Example
 
 ```javascript
-const { QGroupBox } = require("@nodegui/nodegui");
+import { FlexLayout, QGroupBox, QRadioButton, QMainWindow } from './index';
 
-// someone should put a sample code here
-```
+const win = new QMainWindow();
+
+const r1 = new QRadioButton();
+r1.setText('R1');
+const r2 = new QRadioButton();
+r2.setText('R2');
+const r3 = new QRadioButton();
+r3.setText('R3');
+
+const groupBoxLayout = new FlexLayout();
+const groupBox = new QGroupBox();
+groupBox.setLayout(groupBoxLayout);
+groupBoxLayout.addWidget(r1);
+groupBoxLayout.addWidget(r2);
+groupBoxLayout.addWidget(r3);
+
+win.setCentralWidget(groupBox);
+win.resize(400, 400);
+
+win.show();
+(global as any).win = win;```
  */
 
 export class QGroupBox extends NodeWidget<QGroupBoxSignals> {
