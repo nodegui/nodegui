cf7d241b511a3da3ba093b0d242c75700832dfd4
feng8848 <40539968+feng8848@users.noreply.github.com>
2020-02-20 14:53:31 +0800

    Add QTableView support (#406)
    * Add QTableView support
    
    * Fix template to move public expanded methods to private
    
    Co-authored-by: Atul R <atulanand94@gmail.com>

---
+++ src/cpp/include/nodegui/QtWidgets/QTableView/ntableview.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QTableView>
+
+#include "Extras/Utils/nutils.h"
+#include "QtWidgets/QTableView/qtableview_macro.h"
+#include "core/NodeWidget/nodewidget.h"
+
+class DLL_EXPORT NTableView : public QTableView, public NodeWidget {
+  Q_OBJECT
+  NODEWIDGET_IMPLEMENTATIONS(QTableView)
+ public:
+  using QTableView::QTableView;
+
+  void connectSignalsToEventEmitter() { QTABLEVIEW_SIGNALS }
+};
