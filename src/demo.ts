9c8fb852620253fe50f3a45384144ccde1966ccb
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-02-20 12:39:06 +0530

    demo: using the tab component in the demo to verify

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,21 @@
-import { QDateEdit } from './lib/QtWidgets/QDateEdit';
+import { QTabWidget } from './lib/QtWidgets/QTabWidget';
+import { QLabel } from './lib/QtWidgets/QLabel';
+import { QIcon } from './lib/QtGui/QIcon';
 
-const dateEdit = new QDateEdit();
+const icon = new QIcon('/Users/atulr/Project/nodegui/nodegui/src/lib/QtGui/__tests__/assets/nodegui.png');
+const title1 = 'title 1';
+const title2 = 'title 2';
+const tabContent1 = new QLabel();
+const tabContent2 = new QLabel();
 
-dateEdit.show();
+tabContent1.setText('blaaahhhhh');
+tabContent2.setText('blaaahhhhh');
 
-(global as any).dateEdit = dateEdit;
+const tabs = new QTabWidget();
+
+tabs.addTab(tabContent1, icon, title1);
+tabs.addTab(tabContent2, icon, title2);
+
+tabs.show();
+
+(global as any).tabs = tabs;
