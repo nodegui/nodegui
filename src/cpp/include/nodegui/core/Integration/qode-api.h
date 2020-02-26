435846db9a4a8389ee8158c95805d202a9a97cef
master-atul <atulanand94@gmail.com>
2020-02-26 23:06:23 +0100

    Add compilation

--- src/cpp/include/nodegui/core/Integration/qode-api.h
+++ src/cpp/include/nodegui/core/Integration/qode-api.h
@@ -1,8 +1,14 @@
+#ifdef _WIN32
+# define QODE_IMPORT __declspec(dllimport)
+#else
+# define QODE_IMPORT 
+#endif
+
 #pragma once
 // From Qode headers
 namespace qode {
-extern int qode_argc;
-extern char **qode_argv;
-typedef int (*QodeCustomRunLoopFunc)();
-extern void InjectCustomRunLoop(QodeCustomRunLoopFunc customRunLoop);
+QODE_IMPORT extern int qode_argc;
+QODE_IMPORT extern char **qode_argv;
+QODE_IMPORT typedef int (*QodeCustomRunLoopFunc)();
+QODE_IMPORT extern void InjectCustomRunLoop(QodeCustomRunLoopFunc customRunLoop);
 }  // namespace qode
