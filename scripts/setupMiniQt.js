6d6d1b4ff8985b62000962d1648900cb2e34177e
Atul R <atulanand94@gmail.com>
2020-03-07 14:21:51 +0100

    skip setting up miniqt if customQt is on

--- scripts/setupMiniQt.js
+++ scripts/setupMiniQt.js
@@ -3,12 +3,6 @@
 const { setupArtifact } = require('@nodegui/artifact-installer');
 const { miniQt, useCustomQt, qtHome } = require('../config/qtConfig');
 
-if (!useCustomQt) {
-    console.log(`Minimal Qt ${miniQt.version} setup:`);
-} else {
-    console.log(`CustomQt detected at ${qtHome} . Hence, skipping Mini Qt installation...`);
-}
-
 async function setupQt() {
     return Promise.all(
         miniQt.artifacts.map(async artifact =>
@@ -22,7 +16,13 @@ async function setupQt() {
     );
 }
 
-setupQt().catch(err => {
-    console.error(err);
-    process.exit(1);
-});
+if (!useCustomQt) {
+    console.log(`Minimal Qt ${miniQt.version} setup:`);
+
+    setupQt().catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+} else {
+    console.log(`CustomQt detected at ${qtHome} . Hence, skipping Mini Qt installation...`);
+}
