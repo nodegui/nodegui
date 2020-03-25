86642d0ea600c00c9bccbd522fdca55a44de9073
mspencer92 <michaelspencer1992@gmail.com>
2020-03-25 16:53:13 -0400

    Update demo.ts

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,4 @@
-import { QMainWindow, QWidget, FlexLayout, QStandardItemModel, QComboBox } from './index';
-import { CheckState, ItemFlag, ItemDataRole } from './lib/QtEnums';
-import { QStandardItem } from './lib/QtWidgets/QStandardItem';
+import { QMainWindow, QWidget, FlexLayout, QTreeWidgetItem, QTreeWidget, QLineEdit, MatchFlag } from './index';
 
 const win = new QMainWindow();
 const center = new QWidget();
@@ -8,22 +6,52 @@ center.setLayout(new FlexLayout());
 
 win.setCentralWidget(center);
 
-const model = new QStandardItemModel();
+const fruitTree = new QTreeWidget();
+fruitTree.setSortingEnabled(true);
 
-const regionSelection = new QComboBox();
-regionSelection.setModel(model);
-const regions = ['US', 'JP', 'EU', 'ASIA', 'INT'];
-regions.forEach((region, index) => {
-    regionSelection.addItem(undefined, region);
-    const item = model.item(index);
-    if (item instanceof QStandardItem) {
-        item.setFlags(ItemFlag.ItemIsUserCheckable | ItemFlag.ItemIsEnabled);
-        item.setData(CheckState.Checked, ItemDataRole.CheckStateRole);
-    }
+fruitTree.setHeaderLabels(['Fruit', 'Price']);
+
+const fruitObj = [
+    {
+        fruit: 'Banana',
+        price: '2.5',
+    },
+    {
+        fruit: 'Apple',
+        price: '1.0',
+    },
+    {
+        fruit: 'Strawberry',
+        price: '2.5',
+    },
+    {
+        fruit: 'Orange',
+        price: '1.5',
+    },
+];
+
+const items = [];
+
+for (const element of fruitObj) {
+    const fruitItem = new QTreeWidgetItem(fruitTree, [element.fruit, element.price]);
+    items.push(fruitItem);
+}
+
+fruitTree.addTopLevelItems(items);
+
+const filterLineEdit = new QLineEdit();
+filterLineEdit.setPlaceholderText('Filter...');
+filterLineEdit.addEventListener('returnPressed', () => {
+    const filterText = filterLineEdit.text();
+    const foundItems = fruitTree
+        .findItems(filterText, MatchFlag.MatchContains, 0)
+        .concat(fruitTree.findItems(filterText, MatchFlag.MatchContains, 1));
+    fruitTree.topLevelItems.forEach(item => item.setHidden(true));
+    foundItems.forEach(item => item.setHidden(false));
 });
-model.addEventListener('itemChanged', (item: QStandardItem) => console.log(item.checkState()));
-win.layout?.addWidget(regionSelection);
-regionSelection.setEditable(true);
-regionSelection.setEditText('US, JP, EU, ASIA, INT');
+
+center.layout?.addWidget(filterLineEdit);
+center.layout?.addWidget(fruitTree);
+
 win.show();
 (global as any).win = win;
