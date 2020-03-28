d526a474dee08f15fb18d4755875a95577ba5d33
Atul R <atulanand94@gmail.com>
2020-03-28 20:49:38 +0100

    Deploy website version based on 80fe60394fe6d6d6ca0c5c92fc3a20de66149678

---
+++ e2872654.37fcc4d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[285],{386:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"nodegui","name":"nodegui","count":1,"permalink":"/blog/tags/nodegui"}')}}]);
