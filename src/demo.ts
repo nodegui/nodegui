7b7872a38eca6b97f463854b727b217c12cf2cfc
Atul R <atulanand94@gmail.com>
2020-03-28 20:33:29 +0100

    Adds docs for handling events

--- src/demo.ts
+++ src/demo.ts
@@ -1,57 +1,16 @@
-import { QMainWindow, QWidget, FlexLayout, QTreeWidgetItem, QTreeWidget, QLineEdit, MatchFlag } from './index';
+import { QMainWindow, QLabel, WidgetEventTypes, QMouseEvent } from './index';
 
 const win = new QMainWindow();
-const center = new QWidget();
-center.setLayout(new FlexLayout());
 
-win.setCentralWidget(center);
+const label = new QLabel();
+label.setText('Move your move here');
+label.setMouseTracking(true);
 
-const fruitTree = new QTreeWidget();
-fruitTree.setSortingEnabled(true);
-
-fruitTree.setHeaderLabels(['Fruit', 'Price']);
-
-const fruitObj = [
-    {
-        fruit: 'Banana',
-        price: '2.5',
-    },
-    {
-        fruit: 'Apple',
-        price: '1.0',
-    },
-    {
-        fruit: 'Strawberry',
-        price: '2.5',
-    },
-    {
-        fruit: 'Orange',
-        price: '1.5',
-    },
-];
-
-const items = [];
-
-for (const element of fruitObj) {
-    const fruitItem = new QTreeWidgetItem(fruitTree, [element.fruit, element.price]);
-    items.push(fruitItem);
-}
-
-fruitTree.addTopLevelItems(items);
-
-const filterLineEdit = new QLineEdit();
-filterLineEdit.setPlaceholderText('Filter...');
-filterLineEdit.addEventListener('returnPressed', () => {
-    const filterText = filterLineEdit.text();
-    const foundItems = fruitTree
-        .findItems(filterText, MatchFlag.MatchContains, 0)
-        .concat(fruitTree.findItems(filterText, MatchFlag.MatchContains, 1));
-    fruitTree.topLevelItems.forEach(item => item.setHidden(true));
-    foundItems.forEach(item => item.setHidden(false));
+label.addEventListener(WidgetEventTypes.MouseMove, nativeEvt => {
+    const mouseEvt = new QMouseEvent(nativeEvt as any);
+    console.log('mouseMoved at: ', { x: mouseEvt.x(), y: mouseEvt.y() });
 });
 
-center.layout?.addWidget(filterLineEdit);
-center.layout?.addWidget(fruitTree);
-
+win.setCentralWidget(label);
 win.show();
 (global as any).win = win;
