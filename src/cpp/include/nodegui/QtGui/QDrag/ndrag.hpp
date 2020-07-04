5c62cdcc8a3c1908f6d2348cf8e07d4e93824c0a
swittk <switt1995@gmail.com>
2020-07-05 00:58:40 +0700

    Implemented QDrag Class (#626)
    * Added QWheelEvent
    
    * removed x y
    
    * Added QNativeGestureEvent
    
    * Changed wrong type of QNativeGestureEventWrap value
    
    * Added QTabletEvent
    
    * Fixing build error for QTabletEvent
    
    * adding dropaction
    
    * fix typos
    
    * Added more functions to QPainterPath
    
    * Added more functions to QPainterPath
    
    * Fixed multiple typos
    
    * Fixed multiple typos
    
    * Got QPainterPath additions working.
    
    * Modified QPainterPath to use qreal instead
    
    * Added QPointF, added a few missing methods to QPoint
    
    * Added QRectF
    
    * implemented QRectF
    
    * Added acceptDrops and setAcceptDrops to QWidget, will be useful for addon implementation of Drag and Drop
    
    * Added more methods to QUrl
    
    * Added QMimeData, additional methods to QUrl, and Dropping should now be supported
    
    * refactored
    
    * Fixed more merge conflicts
    
    * Is this my final merge conflict??
    
    * All merge conflicts resolved
    
    * All merge conflicts resolved
    
    * Adds guide for drag and drop
    
    * lint fix
    
    * added preliminary stuff, will need to tweak to make compilable..
    
    * Compile passed, let's see if it works...
    
    * QDrag added, working
    
    * pause a bit, QDrag causes delayed error of 'QPaintDevice: Cannot destroy paint device that is being painted'
    
    * Revert "Implemented QDrag class"
    
    * Disabled problematic functions
    
    * pulling from origin
    
    Co-authored-by: Switt Kongdachalert <switt1995@yahoo.com>
    Co-authored-by: Atul R <atulanand94@gmail.com>

---
+++ src/cpp/include/nodegui/QtGui/QDrag/ndrag.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <QDrag>
+
+#include "Extras/Export/export.h"
+#include "QtCore/QObject/qobject_macro.h"
+#include "core/Events/eventwidget.h"
+#include "core/Events/eventwidget_macro.h"
+
+class DLL_EXPORT NDrag : public QDrag, public EventWidget {
+  Q_OBJECT
+  EVENTWIDGET_IMPLEMENTATIONS(QDrag)
+ public:
+  using QDrag::QDrag;
+
+  void connectSignalsToEventEmitter() {
+    QOBJECT_SIGNALS
+    QObject::connect(this, &QDrag::actionChanged, [=](Qt::DropAction action) {
+      Napi::Env env = this->emitOnNode.Env();
+      Napi::HandleScope scope(env);
+      this->emitOnNode.Call(
+          {Napi::String::New(env, "actionChanged"),
+           Napi::Number::From(env, static_cast<int>(action))});
+    });
+  }
+};
