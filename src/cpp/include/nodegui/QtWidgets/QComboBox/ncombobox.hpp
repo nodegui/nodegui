c4d9a93e56ca73ba668e80555c51416e68408b35
Simon Edwards <simon@simonzone.com>
2021-09-25 11:12:03 +0200

    Add many TODO comments, methods, and signals to `QComboBox`

--- src/cpp/include/nodegui/QtWidgets/QComboBox/ncombobox.hpp
+++ src/cpp/include/nodegui/QtWidgets/QComboBox/ncombobox.hpp
@@ -16,6 +16,13 @@ class DLL_EXPORT NComboBox : public QComboBox, public NodeWidget {
     QWIDGET_SIGNALS
     // Qt Connects: Implement all signal connects here
     QObject::connect(
+        this, QOverload<int>::of(&QComboBox::activated), [=](int index) {
+          Napi::Env env = this->emitOnNode.Env();
+          Napi::HandleScope scope(env);
+          this->emitOnNode.Call({Napi::String::New(env, "activated"),
+                                 Napi::Number::From(env, index)});
+        });
+    QObject::connect(
         this, QOverload<int>::of(&QComboBox::currentIndexChanged),
         [=](int index) {
           Napi::Env env = this->emitOnNode.Env();
@@ -36,6 +43,26 @@ class DLL_EXPORT NComboBox : public QComboBox, public NodeWidget {
           Napi::Env env = this->emitOnNode.Env();
           Napi::HandleScope scope(env);
           this->emitOnNode.Call({Napi::String::New(env, "editTextChanged"),
+                                 Napi::String::New(env, text.toStdString())});
+        });
+    QObject::connect(
+        this, QOverload<int>::of(&QComboBox::highlighted), [=](int index) {
+          Napi::Env env = this->emitOnNode.Env();
+          Napi::HandleScope scope(env);
+          this->emitOnNode.Call({Napi::String::New(env, "highlighted"),
+                                 Napi::Number::From(env, index)});
+        });
+    QObject::connect(this, &QComboBox::textActivated, [=](const QString &text) {
+      Napi::Env env = this->emitOnNode.Env();
+      Napi::HandleScope scope(env);
+      this->emitOnNode.Call({Napi::String::New(env, "textActivated"),
+                             Napi::String::New(env, text.toStdString())});
+    });
+    QObject::connect(
+        this, &QComboBox::textHighlighted, [=](const QString &text) {
+          Napi::Env env = this->emitOnNode.Env();
+          Napi::HandleScope scope(env);
+          this->emitOnNode.Call({Napi::String::New(env, "textHighlighted"),
                                  Napi::String::New(env, text.toStdString())});
         });
   }
