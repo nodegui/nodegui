8f35c2fbdd1b3a2ec76e1ff7e61d7aff01fec896
Simon Edwards <simon@simonzone.com>
2023-04-12 22:28:07 +0200

    Move most code into `qsplitter_macro.h` for reuse in subclasses

--- src/cpp/include/nodegui/QtWidgets/QSplitter/nsplitter.hpp
+++ src/cpp/include/nodegui/QtWidgets/QSplitter/nsplitter.hpp
@@ -3,7 +3,7 @@
 #include <QSplitter>
 
 #include "Extras/Export/export.h"
-#include "QtWidgets/QFrame/qframe_macro.h"
+#include "QtWidgets/QSplitter/qsplitter_macro.h"
 #include "core/NodeWidget/nodewidget.h"
 
 class DLL_EXPORT NSplitter : public QSplitter, public NodeWidget {
@@ -12,14 +12,5 @@ class DLL_EXPORT NSplitter : public QSplitter, public NodeWidget {
  public:
   using QSplitter::QSplitter;
 
-  virtual void connectSignalsToEventEmitter() {
-    QFRAME_SIGNALS
-    QObject::connect(this, &QSplitter::splitterMoved, [=](int pos, int index) {
-      Napi::Env env = this->emitOnNode.Env();
-      Napi::HandleScope scope(env);
-      this->emitOnNode.Call({Napi::String::New(env, "splitterMoved"),
-                             Napi::Number::New(env, pos),
-                             Napi::Number::New(env, index)});
-    });
-  }
+  virtual void connectSignalsToEventEmitter() { QSPLITTER_SIGNALS }
 };
