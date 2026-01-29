af039ae75cd3562e5678bca5a00380b60402924c
Simon Edwards <simon@simonzone.com>
2026-01-29 20:02:30 +0100

    Update the Qt install now that the archive layout changed

--- config/qtConfig.js
+++ config/qtConfig.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const SETUP_DIR = path.resolve(__dirname, '..', 'miniqt');
-const QT_VERSION = '6.10.0';
+const QT_VERSION = '6.10.2';
 const MIRROR = Boolean(process.env.QT_LINK_MIRROR) ? process.env.QT_LINK_MIRROR : 'https://download.qt.io';
 
 const checkIfExists = (fullPath) => {
@@ -14,6 +14,7 @@ function getMiniQtConfig() {
     switch (os.platform()) {
         case 'darwin': {
             const qtHome = path.resolve(SETUP_DIR, QT_VERSION, 'macos');
+            const subDir = QT_VERSION + '/macos';
             return {
                 qtHome,
                 artifacts: [
@@ -21,22 +22,26 @@ function getMiniQtConfig() {
                         name: 'Qt Base',
                         link: `${MIRROR}/online/qtsdkrepository/mac_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.clang_64/6.10.2-0-202601261210qtbase-MacOS-MacOS_15-Clang-MacOS-MacOS_15-X86_64-ARM64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'plugins', 'platforms', 'libqcocoa.dylib')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt Svg',
                         link: `${MIRROR}/online/qtsdkrepository/mac_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.clang_64/6.10.2-0-202601261210qtsvg-MacOS-MacOS_15-Clang-MacOS-MacOS_15-X86_64-ARM64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'lib', 'QtSvg.framework', 'QtSvg')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt Tools',
                         link: `${MIRROR}/online/qtsdkrepository/mac_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.clang_64/6.10.2-0-202601261210qttools-MacOS-MacOS_15-Clang-MacOS-MacOS_15-X86_64-ARM64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'bin', 'macdeployqt')),
+                        subDir: subDir,
                     },
                 ],
             };
         }
         case 'win32': {
             const qtHome = path.resolve(SETUP_DIR, QT_VERSION, 'msvc2022_64');
+            const subDir = QT_VERSION + '/msvc2022_64';
             return {
                 qtHome,
                 artifacts: [
@@ -44,22 +49,26 @@ function getMiniQtConfig() {
                         name: 'Qt Base',
                         link: `${MIRROR}/online/qtsdkrepository/windows_x86/desktop/qt6_6102/qt6_6102/qt.qt6.6102.win64_msvc2022_64/6.10.2-0-202601261212qtbase-Windows-Windows_11_24H2-MSVC2022-Windows-Windows_11_24H2-X86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'bin', 'Qt6Core.dll')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt SVG',
                         link: `${MIRROR}/online/qtsdkrepository/windows_x86/desktop/qt6_6102/qt6_6102/qt.qt6.6102.win64_msvc2022_64/6.10.2-0-202601261212qtsvg-Windows-Windows_11_24H2-MSVC2022-Windows-Windows_11_24H2-X86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'bin', 'Qt6Svg.dll')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt Tools',
                         link: `${MIRROR}/online/qtsdkrepository/windows_x86/desktop/qt6_6102/qt6_6102/qt.qt6.6102.win64_msvc2022_64/6.10.2-0-202601261212qttools-Windows-Windows_11_24H2-MSVC2022-Windows-Windows_11_24H2-X86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'bin', 'windeployqt.exe')),
+                        subDir: subDir,
                     },
                 ],
             };
         }
         case 'linux': {
             const qtHome = path.resolve(SETUP_DIR, QT_VERSION, 'gcc_64');
+            const subDir = QT_VERSION + '/gcc_64';
             return {
                 qtHome,
                 artifacts: [
@@ -67,16 +76,19 @@ function getMiniQtConfig() {
                         name: 'Qt Base',
                         link: `${MIRROR}/online/qtsdkrepository/linux_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.linux_gcc_64/6.10.2-0-202601261212qtbase-Linux-RHEL_9_4-GCC-Linux-RHEL_9_4-X86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'bin', 'moc')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt SVG',
                         link: `${MIRROR}/online/qtsdkrepository/linux_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.linux_gcc_64/6.10.2-0-202601261212qtsvg-Linux-RHEL_9_4-GCC-Linux-RHEL_9_4-X86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'lib', 'libQt6Svg.so')),
+                        subDir: subDir,
                     },
                     {
                         name: 'Qt ICU',
                         link: `${MIRROR}/online/qtsdkrepository/linux_x64/desktop/qt6_6102/qt6_6102/qt.qt6.6102.linux_gcc_64/6.10.2-0-202601261212icu-linux-Rhel8.6-x86_64.7z`,
                         skipSetup: checkIfExists(path.resolve(qtHome, 'lib', 'libicuuc.so')),
+                        subDir: subDir,
                     },
                 ],
             };
--- scripts/setupMiniQt.js
+++ scripts/setupMiniQt.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
-
+const path = require('path');
+const { mkdirp } = require('mkdirp');
 const { setupArtifact } = require('@nodegui/artifact-installer');
 const { miniQt, useCustomQt, qtHome } = require('../config/qtConfig');
 
 async function setupQt() {
     return Promise.all(
-        miniQt.artifacts.map(async (artifact) =>
-            setupArtifact({
-                outDir: miniQt.setupDir,
+        miniQt.artifacts.map(async (artifact) => {
+            let outDir = miniQt.setupDir;
+            if (artifact.subDir != null) {
+                outDir = path.join(outDir, artifact.subDir);
+                await mkdirp(outDir);
+            }
+            return setupArtifact({
+                outDir: outDir,
                 id: 'nodegui-mini-qt',
                 displayName: `${artifact.name} for Minimal Qt: ${miniQt.version} installation`,
                 downloadLink: artifact.link,
                 skipSetup: artifact.skipSetup,
-            }),
-        ),
+            });
+        }),
     );
 }
 
